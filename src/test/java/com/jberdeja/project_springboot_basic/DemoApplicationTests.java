6e81bb12b8201f77117d101b70b70831342fd671
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-11-24 00:04:46 -0300

    first commit whit service basic whit methot http get

---
+++ src/test/java/com/jberdeja/project_springboot_basic/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jberdeja.project_springboot_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
